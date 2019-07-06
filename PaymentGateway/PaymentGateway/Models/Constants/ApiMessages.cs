fb882910a1c2051c87587da29b391ea2d0c50998
Varun Horril <varunhorril@outlook.com>
2019-07-06 21:51:41 +0400

    ADD Model: ApiMessages, PaymentViewModel

---
+++ PaymentGateway/PaymentGateway/Models/Constants/ApiMessages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaymentGateway.Models.Constants
+{
+    public static class ApiMessages
+    {
+        public const string MISSING_MERCHANT_ID = "MISSING_MERCHANT_ID";
+        public const string MISSING_CARD_NUMBER = "MISSING_CARD_NUMBER";
+        public const string MISSING_CARD_EXPIRY = "MISSING_CARD_EXPIRY";
+        public const string MISSING_AMOUNT = "MISSING_AMOUNT";
+        public const string MISSING_CURRENCY = "MISSING_CURRENCY";
+        public const string MISSING_CVV_NUMBER = "MISSING_CVV_NUMBER";
+
+        public const string INVALID_MERCHANT_ID = "INVALID_MERCHANT_ID";
+        public const string INVALID_CARD_NUMBER = "INVALID_CARD_NUMBER";
+        public const string INVALID_AMOUNT = "INVALID_AMOUNT";
+
+        public const string AUTH_FAILED = "AUTH_FAILED";
+
+        public const string SERVER_ERROR = "SERVER_ERROR";
+
+    }
+}
---
+++ PaymentGateway/PaymentGateway/Models/ViewModels/PaymentViewModel.cs
@@ -0,0 +1,29 @@
+﻿using PaymentGateway.Attributes;
+using PaymentGateway.Models.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PaymentGateway.Models.ViewModels
+{
+    public class PaymentViewModel
+    {
+        [Required(ErrorMessage = ApiMessages.MISSING_MERCHANT_ID)]
+        [GuidValidation(ErrorMessage = ApiMessages.INVALID_MERCHANT_ID)]
+        public string MerchantId { get; set; }
+
+        [Required(ErrorMessage = ApiMessages.MISSING_CARD_NUMBER)]
+        public string CardNumber { get; set; }
+
+        [Required(ErrorMessage = ApiMessages.MISSING_CARD_EXPIRY)]
+        public string ExpiryMonthDate { get; set; }
+
+        [Required(ErrorMessage = ApiMessages.MISSING_CURRENCY)]
+        public string Currency { get; set; }
+
+        [Required(ErrorMessage = ApiMessages.MISSING_CVV_NUMBER)]
+        public string CvvNumber { get; set; }
+    }
+}
