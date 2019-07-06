a25fa1e0524075c54e3ee209e6becd8714ee0c05
Varun Horril <varunhorril@outlook.com>
2019-07-06 21:52:06 +0400

    ADD Annotation: GuidValidation

---
+++ PaymentGateway/PaymentGateway/Annotations/GuidValidation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PaymentGateway.Attributes
+{
+    public class GuidValidation : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            bool isGuid = Guid.TryParse(value.ToString(), out Guid result);
+            if (isGuid)
+            {
+                return true;
+            }
+            
+            return false;
+        }
+    }
+}
