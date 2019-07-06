22e439ef1c3d1817ff304d13a17e93ebfc210f80
Varun Horril <varunhorril@outlook.com>
2019-07-06 22:21:28 +0400

    ADD Annotation: CVVNumberValidation

---
+++ PaymentGateway/PaymentGateway/Annotations/CVVNumberValidation.cs
@@ -0,0 +1,41 @@
+﻿using PaymentGateway.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PaymentGateway.Annotations
+{
+    public class CVVNumberValidation : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (IsValidNumber(value) && HasValidLength(value))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsValidNumber(object value)
+        {
+            return int.TryParse(value.ToString(), out int result);
+        }
+
+        private bool HasValidLength(object value)
+        {
+            var cvvLength = value.ToString().Length;
+            var maxCvvLength = ConfigHelper.GetCvvMaximumLength();
+            var minCvvLength = ConfigHelper.GetCvvMinimumLength();
+
+            if (cvvLength >= minCvvLength && cvvLength <= maxCvvLength)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
