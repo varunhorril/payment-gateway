67b89af04315a33fb5f91a8b57fabc16bf2985ac
Varun Horril <varunhorril@outlook.com>
2019-07-06 22:21:01 +0400

    ADD Helpers: ConfigHelper, ConfigKeys

---
+++ PaymentGateway/PaymentGateway/Helpers/ConfigHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace PaymentGateway.Helpers
+{
+    public static class ConfigHelper
+    {
+        public static int GetCvvMinimumLength()
+        {
+            int fallbackValue = 3;
+
+            try
+            {
+                var configValue = ConfigurationManager.AppSettings[ConfigKeys.CVV_MIN_LENGTH];
+
+                return Convert.ToInt32(configValue);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return fallbackValue;
+        }
+        public static int GetCvvMaximumLength()
+        {
+            int fallbackValue = 4;
+
+            try
+            {
+                var configValue = ConfigurationManager.AppSettings[ConfigKeys.CVV_MAX_LENGTH];
+
+                return Convert.ToInt32(configValue);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return fallbackValue;
+        }
+    }
+}
---
+++ PaymentGateway/PaymentGateway/Helpers/ConfigKeys.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaymentGateway.Helpers
+{
+    /// <summary>
+    /// Holds keys for AppSettings in Web.config.
+    /// </summary>
+    public static class ConfigKeys
+    {
+        public const string CVV_MIN_LENGTH = "CVV_MIN_LENGTH";
+        public const string CVV_MAX_LENGTH = "CVV_MAX_LENGTH";
+
+
+    }
+}
