819057e8580a09d7696c353017c77be0ea53e5a4
Varun Horril <varunhorril@outlook.com>
2019-07-06 20:29:30 +0400

    ADD Attribute: MerchantValidationAttribute

---
+++ PaymentGateway/PaymentGateway/Attributes/MerchantValidationAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace PaymentGateway.Attributes
+{
+    public class MerchantValidationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            base.OnActionExecuting(actionContext);
+        }
+    }
+}
