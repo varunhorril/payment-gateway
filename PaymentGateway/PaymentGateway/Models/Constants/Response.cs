4771a56a11a9c1b083e9d5b5310ba33252e3973a
Varun Horril <varunhorril@outlook.com>
2019-07-07 09:52:27 +0400

    ADD Response.cs

---
+++ PaymentGateway/PaymentGateway/Models/Constants/Response.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PaymentGateway.Api.Business;
+using PaymentGateway.Api.Business.Interfaces;
+
+namespace PaymentGateway.Models.Constants
+{
+    public class Response : IResponseBase
+    {
+        public bool IsSuccessful { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
