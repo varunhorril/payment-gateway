14af2845d8e89d4730b28e5217c33ec707734afd
Varun Horril <varunhorril@outlook.com>
2019-07-08 01:32:33 +0400

    UPDATE PingController: NLog Temp test.

--- PaymentGateway/PaymentGateway/Controllers/PingController.cs
+++ PaymentGateway/PaymentGateway/Controllers/PingController.cs
@@ -4,16 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using NLog;
 
 namespace PaymentGateway.Controllers
 {
     [Route("")]
     public class PingController : ApiController
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         [Route("Ping")]
         [HttpGet]
         public IHttpActionResult Ping()
         {
+
             return Ok("Service is UP!");
         }
     }
