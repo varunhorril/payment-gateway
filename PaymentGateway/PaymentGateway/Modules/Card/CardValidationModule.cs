8309246dd9cb9509aadf51dd6b7987ce7787bdb3
Varun Horril <varunhorril@outlook.com>
2019-07-07 09:53:18 +0400

    ADD MODULE: CardValidationModule

---
+++ PaymentGateway/PaymentGateway/Modules/Card/CardValidationModule.cs
@@ -0,0 +1,18 @@
+﻿using PaymentGateway.Api.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaymentGateway.Modules.Card
+{
+    public class CardValidationModule : IModuleBase
+    {
+        public string CardNumber { get; set; }
+
+        public IResponseBase Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
