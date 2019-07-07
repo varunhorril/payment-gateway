33dbac8e18461994211b7137f33b0c5e3123a247
Varun Horril <varunhorril@outlook.com>
2019-07-07 09:51:30 +0400

    ADD Interface: IResponseBase, IModuleBase.

---
+++ PaymentGateway/PaymentGateway.Api.Business/Interfaces/IModuleBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Api.Business.Interfaces
+{
+    public interface IModuleBase
+    {
+        IResponseBase Process();
+    }
+}
---
+++ PaymentGateway/PaymentGateway.Api.Business/Interfaces/IResponseBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Api.Business.Interfaces
+{
+    public interface IResponseBase
+    {
+        bool IsSuccessful { get; set; }
+        string Message { get; set; }
+        object Data { get; set; }
+
+    }
+}
