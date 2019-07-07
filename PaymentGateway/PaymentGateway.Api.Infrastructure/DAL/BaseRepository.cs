5207cb80e325c8e311acc36d8eed2a25c0cb6ed3
Varun Horril <varunhorril@outlook.com>
2019-07-07 23:43:22 +0400

    ADD IRepository, BaseRepository.

---
+++ PaymentGateway/PaymentGateway.Api.Infrastructure/DAL/BaseRepository.cs
@@ -0,0 +1,26 @@
+﻿using PaymentGateway.Api.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Api.Infrastructure.DAL
+{
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        public BaseRepository()
+        {
+
+        }
+        public void Insert(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
