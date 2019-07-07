75f933c874a4a46b917a34a367728a2ddb29536d
Varun Horril <varunhorril@outlook.com>
2019-07-07 22:19:39 +0400

    ADD Models in Api.Core

---
+++ PaymentGateway/PaymentGateway.Core/Models/Base/AuditBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Core.Models
+{
+    public class AuditBase
+    {
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
---
+++ PaymentGateway/PaymentGateway/Models/Constants/PaymentRelayStatuses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaymentGateway.Models.Constants
+{
+    public class PaymentRelayStatuses
+    {
+        public const string SUCCESS = "SUCCESS";
+        public const string FAILED = "FAILED";
+    }
+}
