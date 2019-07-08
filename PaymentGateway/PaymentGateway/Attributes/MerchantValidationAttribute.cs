c66b38c9ea730b4f1cd8508567b6dae2720aaf35
Varun Horril <varunhorril@outlook.com>
2019-07-09 00:47:45 +0400

    UPDATE MerchantValidationAttribute: Added call to validation module.

--- PaymentGateway/PaymentGateway/Attributes/MerchantValidationAttribute.cs
+++ PaymentGateway/PaymentGateway/Attributes/MerchantValidationAttribute.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using NLog;
+using PaymentGateway.Models.Constants;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Web;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
@@ -14,7 +20,36 @@ namespace PaymentGateway.Attributes
     {
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
+            var authHeaderValue = actionContext.Request.Headers.Authorization;
+            if (!IsAuthHeaderValid(authHeaderValue))
+            {
+                actionContext.Response.StatusCode = HttpStatusCode.Forbidden;
+                actionContext.Response.Content = new StringContent(ApiMessages.AUTH_FAILED);
+
+                return;
+            }
+
             base.OnActionExecuting(actionContext);
+        }
+
+        private bool IsAuthHeaderValid(AuthenticationHeaderValue auth)
+        {
+            try
+            {
+                var authString = Encoding.UTF8.GetString(Convert.FromBase64String(auth.Parameter));
+                var authValues = authString.Split(new char[] { ':' });
+                if (string.IsNullOrWhiteSpace(authValues[0]) || string.IsNullOrWhiteSpace(authValues[1]))
+                {
+                    return false;
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
