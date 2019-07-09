a9486c92e5166b43d3136c5bdf1006f7d9a7bfe9
Varun Horril <varunhorril@outlook.com>
2019-07-09 21:10:14 +0400

    UPDATE PaymentRelayModule: UpdatePayment in DB added.

--- PaymentGateway/PaymentGateway/Modules/Payment/PaymentRelayModule.cs
+++ PaymentGateway/PaymentGateway/Modules/Payment/PaymentRelayModule.cs
@@ -1,7 +1,19 @@
-﻿using PaymentGateway.Api.Business.Interfaces;
+﻿using Newtonsoft.Json;
+using NLog;
+using PaymentGateway.Api.Business.Interfaces;
+using PaymentGateway.Api.Infrastructure.DAL.Repositories;
+using PaymentGateway.Core.Exceptions;
+using PaymentGateway.Core.Models;
+using PaymentGateway.Helpers;
+using PaymentGateway.Models.Constants;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace PaymentGateway.Modules.Payment
@@ -9,16 +21,70 @@ namespace PaymentGateway.Modules.Payment
     /// <summary>
     /// Sends payment to bank API Endpoint
     /// </summary>
-    public class PaymentRelayModule : IPaymentRelay
+    public class PaymentRelayModule : IPaymentRelayBase
     {
-        public IResponseBase Relay()
+        public Core.Models.Payment Payment { get; set; }
+        public Core.Models.Card Card { get; set; }
+        public Core.Models.Shopper Shopper { get; set; }
+        public Core.Models.Merchant Merchant { get; set; }
+
+
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+        private static readonly HttpClient _httpClient = new HttpClient();
+        private const string BASIC_AUTH_SCHEME = "Basic";
+
+        public async Task<IResponseBase> Relay()
         {
-            throw new NotImplementedException();
+            IResponseBase response = new Response();
+
+            try
+            {
+                using (var httpRequest = new HttpRequestMessage(HttpMethod.Post, GetBankApiEndpoint()))
+                {
+                    httpRequest.Content = new StringContent(GetRequestBody());
+                    _httpClient.DefaultRequestHeaders.Authorization = SetRequestBasicAuth();
+
+                    var httpResponse = await _httpClient.SendAsync(httpRequest);
+
+                    response = HandleResponse(httpResponse);
+                }
+            }
+            catch (WebException webEx)
+            {
+                _logger.Error(webEx, $"[PaymentRelayModule][Relay] REQUEST FAILED: {webEx.Message}");
+            }
+            catch (RelayException ex)
+            {
+                _logger.Error(ex, $"[PaymentRelayModule][Relay] FAILED: {ex.Message}");
+            }
+
+            return response;
         }
 
-        public void SetRequestBasicAuth()
+        public IResponseBase HandleResponse(HttpResponseMessage httpResponse)
         {
-            throw new NotImplementedException();
+            var result = new Response();
+
+            if (!httpResponse.IsSuccessStatusCode)
+            {
+                UpdatePaymentStatus();
+                return result;
+            }
+
+            UpdatePaymentStatus(true);
+            var test = httpResponse.Content.ReadAsStringAsync();
+
+
+            return result;
+        }
+
+        public AuthenticationHeaderValue SetRequestBasicAuth()
+        {
+            var username = ConfigHelper.GetAuthUser();
+            var password = GetAuthPass();
+            var authByteArray = Encoding.ASCII.GetBytes($"{username}:{password}");
+
+            return new AuthenticationHeaderValue(BASIC_AUTH_SCHEME, Convert.ToBase64String(authByteArray));
         }
 
         public string GetBankApiEndpoint()
@@ -31,9 +97,48 @@ namespace PaymentGateway.Modules.Payment
             throw new NotImplementedException();
         }
 
-        public IResponseBase HandleResponse()
+        private string GetRequestBody()
         {
-            throw new NotImplementedException();
+            var paymentRelay = new PaymentRelay()
+            {
+                PaymentId = Payment.PaymentId,
+                CardNumber = Shopper.CardNumber,
+                CardIssuerName = Card.IssuerName,
+                CVVNumber = Shopper.CVVNumber,
+                CardExpiry = Shopper.CardExpiry,
+                Amount = Shopper.Amount,
+                Currency = Shopper.Currency,
+                MerchantName = Merchant.Name,
+                PaymentDate = Payment.CreatedOn
+            };
+
+            var requestBody = JsonConvert.SerializeObject(paymentRelay);
+
+            return requestBody;
+        }
+
+        private string GetAuthPass()
+        {
+            return ConfigHelper.GetAuthPass();
+        }
+
+        private void UpdatePaymentStatus(bool isSuccess = false)
+        {
+            try
+            {
+                var paymentRepository = new PaymentRepository();
+                var payment = paymentRepository.GetById(Payment.PaymentId);
+                payment.PaymentRelayStatus = (isSuccess) ? PaymentRelayStatuses.SUCCESS
+                                                         : PaymentRelayStatuses.FAILED;
+
+                paymentRepository.Update(payment);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, $"[PaymentRelayModule][UpdatePaymentStatus] FAILED : " +
+                                  $"PaymentId: {Payment.PaymentId}" + Environment.NewLine +
+                                  $"{ex.Message}");
+            }
         }
     }
 }
