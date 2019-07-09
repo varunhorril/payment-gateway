baf8583fbd13872cbf846e67c92e931826918e51
Varun Horril <varunhorril@outlook.com>
2019-07-09 16:45:43 +0400

    ADD Annotation: AmountDueValidation

---
+++ PaymentGateway/PaymentGateway/Annotations/AmountDueValidation.cs
@@ -0,0 +1,35 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PaymentGateway.Annotations
+{
+    public class AmountDueValidation : ValidationAttribute
+    {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public override bool IsValid(object value)
+        {
+            try
+            {
+                return IsAmountValid(value);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, $"[AmountDueValidation][IsValid] FAILED: {ex.Message}");
+            }
+
+            return false;
+        }
+
+        private bool IsAmountValid(object value)
+        {
+            var amount = Convert.ToDecimal(value);
+
+            return amount > 0;
+        }
+    }
+}
