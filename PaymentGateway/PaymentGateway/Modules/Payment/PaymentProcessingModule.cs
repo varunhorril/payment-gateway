d460548b255fb5a5cac79df7fa55c08942d92d11
Varun Horril <varunhorril@outlook.com>
2019-07-09 17:55:34 +0400

    UPDATE PaymentProcessingModule: Insert payment in DB.

--- PaymentGateway/PaymentGateway/Modules/Payment/PaymentProcessingModule.cs
+++ PaymentGateway/PaymentGateway/Modules/Payment/PaymentProcessingModule.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using NLog;
 using PaymentGateway.Api.Business.Interfaces;
+using PaymentGateway.Api.Infrastructure.DAL.Repositories;
 using PaymentGateway.Core.Models;
+using PaymentGateway.Models.Constants;
 
 namespace PaymentGateway.Modules.Payment
 {
@@ -12,11 +15,58 @@ namespace PaymentGateway.Modules.Payment
         public Core.Models.Shopper Shopper { get; set; }
         public Core.Models.Merchant Merchant { get; set; }
         public Core.Models.Bank Bank { get; set; }
-        public Core.Models.Card Card { get; set; }
+
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        private PaymentRepository paymentRepository = new PaymentRepository();
 
         public IResponseBase Process()
         {
-            throw new NotImplementedException();
+            var response = new Response();
+
+            try
+            {
+                var paymentId = Guid.NewGuid();
+                var payment = new Core.Models.Payment()
+                {
+                    PaymentId = paymentId,
+                    BankId = Bank.BankId,
+                    ShopperId = Shopper.ShopperId,
+                    PaymentRelayStatus = PaymentRelayStatuses.PENDING
+                };
+
+                paymentRepository.Insert(payment);
+
+                var storedPayment = GetPayment(paymentId);
+                if (storedPayment != null)
+                {
+                    response.Data = storedPayment;
+                    response.IsSuccessful = true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, $"[PaymentProcessingModule][Process] FAILED: {ex.Message}");
+            }
+
+            return response;
+        }
+
+        private Core.Models.Payment GetPayment(Guid paymentId)
+        {
+            try
+            {
+                var payment = paymentRepository.GetById(paymentId);
+
+                return payment;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, $"[PaymentProcessingModule][GetPayment] FAILED: {ex.Message}");
+            }
+
+            return null;
         }
     }
 }
