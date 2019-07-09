874b8ee51c395d0fbe8c151e07d6d3e2732da933
Varun Horril <varunhorril@outlook.com>
2019-07-09 21:09:16 +0400

    UPDATE IPaymentRelay: Set async functions

--- PaymentGateway/PaymentGateway.Api.Business/Interfaces/IPaymentRelay.cs
+++ PaymentGateway/PaymentGateway.Api.Business/Interfaces/IPaymentRelay.cs
@@ -1,19 +1,17 @@
 ﻿using PaymentGateway.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace PaymentGateway.Api.Business.Interfaces
 {
-    public interface IPaymentRelay
+    public interface IPaymentRelayBase
     {
         Payment Payment { get; set; }
-        IResponseBase Relay();
-        IResponseBase HandleResponse();
+        Task<IResponseBase> Relay();
+        IResponseBase HandleResponse(HttpResponseMessage httpResponse);
         string GetBankApiEndpoint();
         string EncryptRequestContent();
-        void SetRequestBasicAuth();
+        AuthenticationHeaderValue SetRequestBasicAuth();
     }
 }
