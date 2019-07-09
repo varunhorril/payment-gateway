4c241248ddec0d20ccbd6efb658210aa56dedae4
Varun Horril <varunhorril@outlook.com>
2019-07-09 18:27:44 +0400

    ADD MODEL: PaymentRelay for API Client request.

---
+++ PaymentGateway/PaymentGateway.Core/Models/PaymentRelay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Core.Models
+{
+    public class PaymentRelay
+    {
+        public Guid PaymentId { get; set; }
+        public string CardNumber { get; set; }
+        public string CVVNumber { get; set; }
+        public string CardExpiry { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public string MerchantName { get; set; }
+        public string CardIssuerName { get; set; }
+        public DateTime PaymentDate { get; set; }
+    }
+}
