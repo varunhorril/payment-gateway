5815e8359b038715a2e631f04aa70338c4615f60
Varun Horril <varunhorril@outlook.com>
2019-07-09 16:45:08 +0400

    ADD Interface: IPaymentRelay

---
+++ PaymentGateway/PaymentGateway.Api.Business/Interfaces/IPaymentRelay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Api.Business.Interfaces
+{
+    public interface IPaymentRelay
+    {
+        IResponseBase Relay();
+        IResponseBase HandleResponse();
+        string GetBankApiEndpoint();
+        string EncryptRequestContent();
+        void SetRequestBasicAuth();
+    }
+}
