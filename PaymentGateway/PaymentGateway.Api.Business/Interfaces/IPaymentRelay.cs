9a2c1b07a67462cb7dfeb5f6806420ba118dbc45
Varun Horril <varunhorril@outlook.com>
2019-07-09 17:46:34 +0400

    UPDATE IPaymentRelay

--- PaymentGateway/PaymentGateway.Api.Business/Interfaces/IPaymentRelay.cs
+++ PaymentGateway/PaymentGateway.Api.Business/Interfaces/IPaymentRelay.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PaymentGateway.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace PaymentGateway.Api.Business.Interfaces
 {
     public interface IPaymentRelay
     {
+        Payment Payment { get; set; }
         IResponseBase Relay();
         IResponseBase HandleResponse();
         string GetBankApiEndpoint();
