b0f6dc7b71de229307e725c4a5e803ad66fa0618
Varun Horril <varunhorril@outlook.com>
2019-07-09 16:48:59 +0400

    ADD MODULE: PaymentProcessingModule & PaymentRelayModule.cs

---
+++ PaymentGateway/PaymentGateway/Modules/Payment/PaymentProcessingModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PaymentGateway.Api.Business.Interfaces;
+using PaymentGateway.Core.Models;
+
+namespace PaymentGateway.Modules.Payment
+{
+    public class PaymentProcessingModule : IModuleBase
+    {
+        public Core.Models.Shopper Shopper { get; set; }
+        public Core.Models.Merchant Merchant { get; set; }
+        public Core.Models.Bank Bank { get; set; }
+        public Core.Models.Card Card { get; set; }
+
+        public IResponseBase Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PaymentGateway/PaymentGateway/Modules/Payment/PaymentRelayModule.cs
@@ -0,0 +1,39 @@
+﻿using PaymentGateway.Api.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaymentGateway.Modules.Payment
+{
+    /// <summary>
+    /// Sends payment to bank API Endpoint
+    /// </summary>
+    public class PaymentRelayModule : IPaymentRelay
+    {
+        public IResponseBase Relay()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetRequestBasicAuth()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetBankApiEndpoint()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string EncryptRequestContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResponseBase HandleResponse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
