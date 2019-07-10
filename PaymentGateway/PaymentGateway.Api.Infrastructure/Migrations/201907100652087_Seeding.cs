36daf0e35c6060eb5446717e6c507620d0ab8f3f
Varun Horril <varunhorril@outlook.com>
2019-07-10 10:54:28 +0400

    ADD Migration: Seeding DB.

---
+++ PaymentGateway/PaymentGateway.Api.Infrastructure/Migrations/201907100652087_Seeding.cs
@@ -0,0 +1,16 @@
+namespace PaymentGateway.Api.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Seeding : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
