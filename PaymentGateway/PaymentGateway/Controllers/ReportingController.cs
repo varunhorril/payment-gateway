5316341fcf722dedda753b83e8b14da68bec40cb
Varun Horril <varunhorril@outlook.com>
2019-07-11 00:47:00 +0400

    UPDATE ReportingController: Added call to ReportModule & reponse handling

--- PaymentGateway/PaymentGateway/Controllers/ReportingController.cs
+++ PaymentGateway/PaymentGateway/Controllers/ReportingController.cs
@@ -1,7 +1,12 @@
 ﻿using NLog;
+using PaymentGateway.Attributes;
+using PaymentGateway.Models.Constants;
+using PaymentGateway.Models.ViewModels;
+using PaymentGateway.Modules.Report;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Http;
 
@@ -14,21 +19,35 @@ namespace PaymentGateway.Controllers
 
         [HttpPost]
         [Route("Report")]
-        public IHttpActionResult RetrievePayment()
+        [MerchantValidation]
+        public IHttpActionResult RetrievePayment(ReportRequestViewModel report)
         {
+            var apiResponse = new ApiResponse();
 
             try
             {
+                var reportProcessModule = new ReportModule()
+                {
+                    PaymentId = report.PaymentId
 
+                }.Process();
 
+                if (reportProcessModule.IsSuccessful)
+                {
+                    apiResponse.Content = (string)reportProcessModule.Data;
+                    apiResponse.StatusCode = HttpStatusCode.OK;
 
+                    return apiResponse;
+                }
             }
             catch (Exception ex)
             {
                 _logger.Error(ex, $"[ReportingController][RetrievePayment] : {ex.Message}");
             }
 
-            return null;
+            apiResponse.StatusCode = HttpStatusCode.InternalServerError;
+
+            return apiResponse;
         }
     }
 }
