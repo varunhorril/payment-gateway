f0be6f5cc7c45875f59f9a380a3f4c68ed8e397e
Varun Horril <varunhorril@outlook.com>
2019-07-11 00:44:40 +0400

    ADD ReportVM & ReportResponseVM

---
+++ PaymentGateway/PaymentGateway/Models/ViewModels/ReportResponseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaymentGateway.Models.ViewModels
+{
+    public class ReportResponseViewModel
+    {
+        public Guid PaymentId { get; set; }
+        public string MaskedCardNumber { get; set; }
+        public string CVVNumber { get; set; }
+        public string CardExpiry { get; set; }
+        public string PaymentProcessStatus { get; set; }
+        public string DateProcessed { get; set; }
+    }
+}
---
+++ PaymentGateway/PaymentGateway/Models/ViewModels/ReportViewModel.cs
@@ -0,0 +1,17 @@
+﻿using PaymentGateway.Attributes;
+using PaymentGateway.Models.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PaymentGateway.Models.ViewModels
+{
+    public class ReportRequestViewModel
+    {
+        [Required(ErrorMessage = ApiMessages.MISSING_PAYMENT_ID)]
+        [GuidValidation(ErrorMessage = ApiMessages.INVALID_PAYMENT_ID)]
+        public string PaymentId { get; set; }
+    }
+}
