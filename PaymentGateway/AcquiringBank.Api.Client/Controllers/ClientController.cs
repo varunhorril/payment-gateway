80e19220bad96876f5b0fe1a3f50b534b6de551d
Varun Horril <varunhorril@outlook.com>
2019-07-10 21:47:35 +0400

    UPDATE ClientController: BankProcess completed + Return content

--- PaymentGateway/AcquiringBank.Api.Client/Controllers/ClientController.cs
+++ PaymentGateway/AcquiringBank.Api.Client/Controllers/ClientController.cs
@@ -1,8 +1,13 @@
-﻿using AcquiringBank.Api.Client.Models;
+﻿using AcquiringBank.Api.Client.Attributes;
+using AcquiringBank.Api.Client.Models;
+using AcquiringBank.Api.Client.Models.Constants;
+using Newtonsoft.Json;
 using NLog;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Http;
 
@@ -13,23 +18,61 @@ namespace AcquiringBank.Api.Client.Controllers
     {
         private static Logger _errorLogger = LogManager.GetLogger("logfile");
         private static Logger _bankLogger = LogManager.GetLogger("transactionfile");
+        private string TransactionId;
 
         [Route("Process")]
         [HttpPost]
-        public IHttpActionResult ProcessTransaction()
+        [PaymentGatewayValidation]
+        public IHttpActionResult ProcessTransaction(BankProcessViewModel bankModel)
         {
             var clientResponse = new ApiClientResponse();
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    clientResponse.StatusCode = HttpStatusCode.BadRequest;
+                    clientResponse.Content = ApiMessages.FAIL;
 
+                    return clientResponse;
+                }
+
+                TransactionId = Guid.NewGuid().ToString();
+                var processed = IsProcessingComplete(bankModel);
+                clientResponse.StatusCode = (processed) ? HttpStatusCode.OK : HttpStatusCode.InternalServerError;
+                clientResponse.Content = (processed) ? TransactionId : ApiMessages.SERVER_ERROR;
 
             }
             catch (Exception ex)
             {
                 _errorLogger.Error(ex, $"[ClientController][ProcessTransaction] {ex.Message}");
+                clientResponse.StatusCode = HttpStatusCode.InternalServerError;
+                clientResponse.Content = ApiMessages.SERVER_ERROR;
             }
 
             return clientResponse;
         }
+
+        private bool IsProcessingComplete(BankProcessViewModel dataModel)
+        {
+            try
+            {
+                var receivedData = JsonConvert.SerializeObject(dataModel);
+                _bankLogger.Info(Environment.NewLine + Environment.NewLine
+                                + DateTime.UtcNow.ToLongDateString()
+                                + Environment.NewLine
+                                + $"[PAYMENT RECEIVED][{TransactionId}] "
+                                + Environment.NewLine
+                                + receivedData);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _errorLogger.Error(ex);
+            }
+
+            return false;
+        }
+
     }
 }
