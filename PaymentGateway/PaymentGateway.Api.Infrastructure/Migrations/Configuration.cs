b4d365e570cdad9e3ea94b07783b84f97257a5ee
Varun Horril <varunhorril@outlook.com>
2019-07-10 09:01:09 +0400

    UPDATE EF Configurations

--- PaymentGateway/PaymentGateway.Api.Infrastructure/Migrations/Configuration.cs
+++ PaymentGateway/PaymentGateway.Api.Infrastructure/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ namespace PaymentGateway.Api.Infrastructure.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<PaymentGateway.Api.Infrastructure.DAL.PaymentGatewayContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DAL.PaymentGatewayContext>
     {
         public Configuration()
         {
