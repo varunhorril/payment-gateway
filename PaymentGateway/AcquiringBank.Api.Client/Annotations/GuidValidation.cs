e6831e129cb00c88096c54e11fc7701ede615188
Varun Horril <varunhorril@outlook.com>
2019-07-10 21:44:56 +0400

    ADD Annotation: GuidValidation

---
+++ PaymentGateway/AcquiringBank.Api.Client/Annotations/GuidValidation.cs
@@ -0,0 +1,23 @@
+﻿using AcquiringBank.Api.Client.Models.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AcquiringBank.Api.Client.Annotations
+{
+    public class GuidValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            bool isGuid = Guid.TryParse(value.ToString(), out Guid result);
+            if (isGuid)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(ApiMessages.FAIL);
+        }
+    }
+}
