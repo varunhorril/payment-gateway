cc1193074a64e0ce0a9c2bf45a2f239703073d7e
Varun Horril <varunhorril@outlook.com>
2019-07-10 19:46:56 +0400

    ADD MODEL: ApiClientResponse & ApiMessage.

---
+++ PaymentGateway/AcquiringBank.Api.Client/Models/ApiClientResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace AcquiringBank.Api.Client.Models
+{
+    public class ApiClientResponse : IHttpActionResult
+    {
+        public string Content { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var httpResponse = new HttpResponseMessage()
+            {
+                Content = new StringContent(Content, Encoding.UTF8, "application/json"),
+                StatusCode = StatusCode
+            };
+
+            return Task.FromResult(httpResponse);
+        }
+    }
+}
---
+++ PaymentGateway/AcquiringBank.Api.Client/Models/Constants/ApiMessages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcquiringBank.Api.Client.Models.Constants
+{
+    public static class ApiMessages
+    {
+        public const string OK = "OK";
+        public const string FAIL = "FAIL";
+        public const string SERVER_ERROR = "SERVER_ERROR";
+        public const string AUTH_FAILED = "AUTH_FAILED";
+        public const string INVALID_REQUEST = "INVALID_REQUEST";
+    }
+}
