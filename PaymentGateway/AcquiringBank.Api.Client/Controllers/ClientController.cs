681ec4a046bf9246cbc0e8a58d5ad8d32d5d4f59
Varun Horril <varunhorril@outlook.com>
2019-07-10 19:47:31 +0400

    ADD ClientController: Process API Endpoint.

---
+++ PaymentGateway/AcquiringBank.Api.Client/Controllers/ClientController.cs
@@ -0,0 +1,35 @@
+﻿using AcquiringBank.Api.Client.Models;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AcquiringBank.Api.Client.Controllers
+{
+    [Route("")]
+    public class ClientController : ApiController
+    {
+        private static Logger _errorLogger = LogManager.GetLogger("logfile");
+        private static Logger _bankLogger = LogManager.GetLogger("transactionfile");
+
+        [Route("Process")]
+        [HttpPost]
+        public IHttpActionResult ProcessTransaction()
+        {
+            var clientResponse = new ApiClientResponse();
+            try
+            {
+
+
+            }
+            catch (Exception ex)
+            {
+                _errorLogger.Error(ex, $"[ClientController][ProcessTransaction] {ex.Message}");
+            }
+
+            return clientResponse;
+        }
+    }
+}
